b7be7dc936876bc60911eedf4b11089bc6a09ea5
RICHARDKALVIN <riczyrichard@gmail.com>
2025-08-10 18:04:18 +0530

    generating html for order summary

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,17 @@
-export const cart=[];
+export const cart=[
+    {
+        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity :2
+    },
+    {
+        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity :2
+    },
+    {
+        productId : "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+        quantity :2
+    }
+];
 
 export function addToCart(productId,selectedValue){
    let isnotExist =true;
